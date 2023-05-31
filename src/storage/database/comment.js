988f8ced9b5a15aacd22acda545ef3740bd7a48e
Apoorva Chauhan <47474447+apoorvacha@users.noreply.github.com>
2023-06-01 04:39:27 +0530

    Add files via upload

---
+++ src/storage/database/comment.js
@@ -0,0 +1,27 @@
+const comment = [
+  {
+    id: 1,
+    image: require('../images/store2.jpg'),
+    comment: 'Great',
+    user: 'Robin',
+  },
+  {
+    id: 2,
+    image: require('../images/profil3.jpg'),
+    comment: 'Cool',
+    user: 'Barney',
+  },
+  {
+    id: 3,
+    image: require('../images/profil4.jpg'),
+    comment: ':))))',
+    user: 'Ted',
+  },
+  {
+    id: 4,
+    comment: 'Nice..',
+    image: require('../images/profil6.jpg'),
+    user: 'Marshall',
+  },
+];
+export default comment;
---
+++ src/storage/database/message.js
@@ -0,0 +1,59 @@
+const message = [
+  {
+    id: '1',
+    user: 'Barney',
+    username: 'Barney',
+    message: 'Legendary',
+    image: require('../images/profil2.jpg'),
+  },
+  {
+    id: '2',
+    user: 'Robin',
+    username: 'Robin',
+    message: 'Exciting',
+    image: require('../images/profil3.jpg'),
+  },
+  {
+    id: '3',
+    user: 'Ted',
+    username: 'Ted',
+    message: 'Great',
+    image: require('../images/profil4.jpg'),
+  },
+  {
+    id: '4',
+    user: 'Marshall',
+    username: 'Marshall',
+    message: 'Good stuff',
+    image: require('../images/profil5.jpg'),
+  },
+  {
+    id: '5',
+    user: 'Ranjit',
+    username: 'Ranjit',
+    message: 'Coming up',
+    image: require('../images/profil6.jpg'),
+  },
+  {
+    id: '6',
+    user: 'Burak Ceylan',
+    username: 'burakceylan',
+    message: 'Bir mesajı beğendi · 8s',
+    image: require('../images/profil2.jpg'),
+  },
+  {
+    id: '7',
+    user: 'Ahmet Ok',
+    username: 'ahmettok',
+    message: 'Çok komik bir video · 12s',
+    image: require('../images/profil3.jpg'),
+  },
+  {
+    id: '8',
+    user: 'Busra Ceylan',
+    username: 'buscey',
+    message: 'Bir reels videosu paylaştı · 18s',
+    image: require('../images/profil4.jpg'),
+  },
+];
+export default message;
